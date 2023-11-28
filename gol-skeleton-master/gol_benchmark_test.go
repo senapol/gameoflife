93fc12bd063a04b3d746b5adb68cde7b5f7e88e4
senapol <127954337+senapol@users.noreply.github.com>
2023-11-28 18:09:47 +0000

    Update gol_benchmark_test.go

--- gol-skeleton-master/gol_benchmark_test.go
+++ gol-skeleton-master/gol_benchmark_test.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"uk.ac.bris.cs/gameoflife/gol"
 )
 
 func BenchmarkRun(b *testing.B) {
+
+	os.Stdout = nil
+
 	fmt.Println("got here")
-	for threads := 1; threads <= 16; threads *= 2 {
+	for threads := 1; threads <= 16; threads++ {
 		b.Run(fmt.Sprintf("%d_threads", threads), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				fmt.Println("stuck in loop")
